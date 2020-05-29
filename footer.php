67162e2f185d50cf286c4bc59f6319f8022b0d68
unknown <mylesfoster1989@gmail.com>
2020-05-29 16:07:56 +1200

    casestudy1

--- footer.php
+++ footer.php
@@ -1,2 +1,3 @@
-<div id="footer">&copy; Animals, Unitec, NZ</div>
+<div id="footer">
+    &copy; Animals, Unitec, NZ</div>
         </div>
---
+++ header.php
@@ -0,0 +1,3 @@
+<div id = "header">
+    <img id ="leftDivPic" src="image/animal logo.PNG" /></div>
+</div>
